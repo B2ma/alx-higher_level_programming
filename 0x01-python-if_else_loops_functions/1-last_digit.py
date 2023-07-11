40b75a097e9c7152417915ac4b6722d106c4d257
B2ma <beatrice.terer@gmail.com>
2023-07-11 11:18:48 +0000

    Corrected pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 statement1 = f"Last digit of {number} is "
-statement2 =f"{-int(str(number)[-1]) if number < 0 else int(str(number)[-1])}"
+statement2 = f"{-int(str(number)[-1]) if number < 0 else int(str(number)[-1])}"
 if int(str(number)[-1]) > 5 and number > 0:
     print(f"{statement1}{statement2 } and is greater than 5")
 elif int(str(number)[-1]) == 0:
