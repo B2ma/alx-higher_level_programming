9668242faa009c9e8fb6e5c89367c22a73b453bb
B2ma <beatrice.terer@gmail.com>
2023-07-11 11:15:17 +0000

    Amended 1-last_digit.py to accomodate when the last digit is greater than 6 and the number is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-the_statement = f"Last digit of {number} is {-int(str(number)[-1]) if number < 0 else int(str(number)[-1])}"
-if int(str(number)[-1]) > 5:
-    print(f"{the_statement } and is greater than 5")
+statement1 = f"Last digit of {number} is "
+statement2 =f"{-int(str(number)[-1]) if number < 0 else int(str(number)[-1])}"
+if int(str(number)[-1]) > 5 and number > 0:
+    print(f"{statement1}{statement2 } and is greater than 5")
 elif int(str(number)[-1]) == 0:
-    print(f"{the_statement } and is 0")
-elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
-    print(f"{the_statement } and is less than 6 and not 0")
+    print(f"{statement1}{statement2 } and is 0")
+elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0 and number > 0:
+    print(f"{statement1}{statement2 } and is less than 6 and not 0")
+elif int(str(number)[-1]) > 6 and int(str(number)[-1]) != 0 and number < 0:
+    print(f"{statement1}{statement2 } and is less than 6 and not 0")
+elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0 and number < 0:
+    print(f"{statement1}{statement2 } and is less than 6 and not 0")
