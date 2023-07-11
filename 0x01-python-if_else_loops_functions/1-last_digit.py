55063ce191458c7f39d6663ac0399f25251afbbd
B2ma <beatrice.terer@gmail.com>
2023-07-11 10:04:27 +0000

    Created 1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+the_statement = f"Last digit of {number} is {str(number)[-1]}"
+if int(str(number)[-1]) > 5:
+    print(f"{the_statement } and is greater than 5")
+elif int(str(number)[-1]) == 0:
+    print(f"{the_statement } and is 0")
+elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
+    print(f"{the_statement } and is less than 6 and not 0")
