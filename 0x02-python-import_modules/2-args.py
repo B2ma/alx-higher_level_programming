27148699638476561466d7448b8c3f6c0c8aa375
B2ma <beatrice.terer@gmail.com>
2023-07-13 15:27:00 +0000

    updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-args_num = len(arguments)
-print("{} argument{}".format(args_num, 's.' if args_num == 0 else ':' if
-                             args_num == 1 else 's:'))
-for i, arg in enumerate(arguments, start=1):
-    print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    args_num = len(arguments)
+    print("{} argument{}".format(args_num, 's.' if args_num == 0 else ':' if
+                                  args_num == 1 else 's:'))
+    for i, arg in enumerate(arguments, start=1):
+        print("{}: {}".format(i, arg))
