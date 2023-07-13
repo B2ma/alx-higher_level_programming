f4e11fb1dada8a422d23fc1648fe04fe690f126b
B2ma <beatrice.terer@gmail.com>
2023-07-13 13:42:47 +0000

    Created 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calc
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(int(a), int(b), int(calc.add(a, b))))
+    print("{} - {} = {}".format(int(a), int(b), int(calc.sub(a, b))))
+    print("{} * {} = {}".format(int(a), int(b), int(calc.mul(a, b))))
+    print("{} / {} = {}".format(int(a), int(b), int(calc.div(a, b))))
