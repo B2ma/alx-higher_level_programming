65914229cda08fef497ea385ea111f2bc800c0ea
B2ma <beatrice.terer@gmail.com>
2023-07-17 18:42:46 +0000

    modified 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,75 +1,50 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
-* reverse_listint - Reverses a singly-linked listint_t list.
-* @head: A pointer to the starting node of the list to reverse.
-*
-* Return: A pointer to the head of the reversed list.
-*/
-void reverse_listint(listint_t **head)
-{
-listint_t *prev = NULL;
-listint_t *current = *head;
-listint_t *next = NULL;
-
-while (current != NULL)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-
-*head = prev;
-}
-
-/**
-* is_palindrome - Checks if a singly linked list is a palindrome.
-* @head: A pointer to the head of the linked list.
-* Return: 1, 0 otherwise.
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: pointer to head pointer
+* Return: 1, 0 otherwise
 */
 int is_palindrome(listint_t **head)
 {
-if (head == NULL || *head == NULL)
-return (0);
-listint_t *slow = *head, *fast = *head, *prev_slow = *head;
-listint_t *mid = NULL;
-int result = 1;
+listint_t *slow = *head, *fast = *head;
+listint_t *previous = NULL, *temp = NULL;
+int is_palindrome = 1;
 
+if (head == NULL || (*head)->next == NULL)
+return (0);
 while (fast != NULL && fast->next != NULL)
 {
 fast = fast->next->next;
-prev_slow = slow, slow = slow->next;
-}
-if (fast != NULL)
-{
-mid = slow;
+temp = slow;
 slow = slow->next;
 }
-prev_slow->next = NULL;
-reverse_listint(&slow);
-listint_t *temp1 = *head, *temp2 = slow;
-while (temp1 != NULL && temp2 != NULL)
+if (fast != NULL)
+slow = slow->next;
+while (slow != NULL)
 {
-if (temp1->n != temp2->n)
+temp = slow->next;
+slow->next = previous;
+previous = slow, slow = temp;
+}
+slow = *head;
+while (previous != NULL)
 {
-result = 0;
+if (slow->n != previous->n)
+{
+is_palindrome = 0;
 break;
 }
-temp1 = temp1->next;
-temp2 = temp2->next;
+slow = slow->next, previous = previous->next;
 }
-reverse_listint(&slow);
-if (mid != NULL)
+previous = NULL;
+while (*head != NULL)
 {
-prev_slow->next = mid;
-mid->next = slow;
+temp = (*head)->next;
+(*head)->next = previous;
+previous = *head, *head = temp;
 }
-else
-{
-prev_slow->next = slow;
-}
-return (result);
+*head = previous;
+return (is_palindrome);
 }
