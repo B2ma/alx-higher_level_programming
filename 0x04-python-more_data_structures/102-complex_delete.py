3778e96bd872ffc46650050987835cc1e96c733d
B2ma <beatrice.terer@gmail.com>
2023-07-19 15:12:17 +0000

    Changed the code

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,9 @@
 #!/usr/python3
 def complex_delete(a_dictionary, value):
-    keystodelete = [key for key, val in a_dictionary.items() if val == value]
-    for key in keystodelete:
-        del a_dictionary[key]
-    return a_dictionary
+    while value in a_dictionary.values():
+        for key, val in a_dictionary.items():
+            if val == value:
+                del a_dictionary[key]
+                break
+
+    return (a_dictionary)
