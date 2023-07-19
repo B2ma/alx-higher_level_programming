b4f84daea2a9b12a1d08e69363a5c64a5c662b23
B2ma <beatrice.terer@gmail.com>
2023-07-19 16:15:41 +0000

    ammended the code

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/python3
 def complex_delete(a_dictionary, value):
-    dict_copy = a_dictionary.copy()
-    for i in dict_copy:
-        if dict_copy[i] == value:
+    copy = a_dictionary.copy()
+    for i in copy:
+        if copy[i] == value:
             del a_dictionary[i]
     return a_dictionary
