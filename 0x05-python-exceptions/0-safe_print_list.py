d390a591b25f198f886620988a79c922fb617e66
B2ma <beatrice.terer@gmail.com>
2023-07-31 15:40:30 +0000

    Corrected the first line of code in 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """Prints x elements of a list
-    Args:
-        my_list(list): the list to print elements
-        x: Number of elements of my_list to print.
-    Returns:
-        the number of elements printed
-    """
     i = 0
     count = 0
     for i in range(0, x):
