1526482398c796a91213ac1a7d8a402ef3b5902d
B2ma <beatrice.terer@gmail.com>
2023-07-31 13:43:46 +0000

    Updated 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,8 @@ def safe_print_list(my_list=[], x=0):
                 i += 1
     except TypeError as e:
         print("Error: ", e)
+    except IndexError:
+        pass
     finally:
         print()
     return i
