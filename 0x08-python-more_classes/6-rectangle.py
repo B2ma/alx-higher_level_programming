72838771d4e030e0b22aaa99869b3cae20e180d9
B2ma <beatrice.terer@gmail.com>
2023-08-07 12:39:32 +0000

    Updated 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
 
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -110,3 +111,4 @@ class Rectangle:
         Deletes an instance of rectangle and prints a message
         """
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
