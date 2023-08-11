b8730320f6a5cbdad9351c056ddf5b9f457ca713
B2ma <beatrice.terer@gmail.com>
2023-08-11 20:22:22 +0000

    updated 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -24,6 +24,6 @@ def say_my_name(first_name, last_name=""):
         if last_name:
             print("My name is {} {}".format(first_name, last_name))
         else:
-            print("My name is {}".format(first_name))
+            print("My name is {} ".format(first_name),)
     except Exception as e:
         print(e)
