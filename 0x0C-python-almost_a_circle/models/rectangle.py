6991b862045de83784df674b24fa28eb3c0eb1eb
B2ma <beatrice.terer@gmail.com>
2023-08-18 12:02:20 +0000

    Updated the display in Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,7 +109,10 @@ class Rectangle(Base):
         """
         prints in stdout the Rectangle instance with the character #
         """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
+            print(" " * self.x, end="")
             for _ in range(self.width):
                 print("#", end="")
             print()
