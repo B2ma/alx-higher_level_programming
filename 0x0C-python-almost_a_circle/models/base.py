ea5bff0498f043cce11f93b1207ca88ac1ac1da1
B2ma <beatrice.terer@gmail.com>
2023-09-08 13:41:49 +0000

    Updated base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The base class Module"""
 import json
+import csv
 
 
 class Base:
@@ -127,5 +128,39 @@ class Base:
                 json_string = myfile.read()
                 obj_list = cls.from_json_string(json_string)
                 return [cls.create(**obj) for obj in obj_list]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow(
+                        [obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        obj_list = []
+        try:
+            with open(filename, mode="r", encoding="utf-8") as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(
+                                int(row[1]),
+                                int(row[2]),
+                                int(row[3]), int(row[4]), int(row[0]))
+                    elif cls.__name__ == "Square":
+                        obj = cls(
+                                int(row[1]),
+                                int(row[2]), int(row[3]), int(row[0]))
+                    obj_list.append(obj)
+            return obj_list
         except FileNotFoundError:
             return []
