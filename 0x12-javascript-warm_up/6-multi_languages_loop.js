42716c1529f0b2dfcbf96cdada5daf9b14124141
B2ma <beatrice.terer@gmail.com>
2023-10-16 17:02:22 +0000

    Created 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
+while (index < myArray.length) {
+  console.log(myArray[index]);
+  index++;
+}
