95a875580667e7e847cfb81f7335dd5d51ac2061
B2ma <beatrice.terer@gmail.com>
2023-10-17 18:24:38 +0000

    Updated 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (number) {
-    if (number === 0) {
-      return '';
-    } else {
-      return exports.converter(base)(Math.floor(number / base)) + (number % base).toString(base).toUpperCase();
-    }
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
