426e05df1168c232acf23cf91d1f7dfac268849f
B2ma <beatrice.terer@gmail.com>
2023-10-21 20:01:12 +0000

    Updated 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -28,7 +28,7 @@ def list_states(username, password, database_name):
         cursor = connection.cursor()
         query = "SELECT *\
                  FROM `states`\
-                 WHERE name LIKE BINARY 'N%'\
+                 WHERE name LIKE 'N%'\
                  ORDER BY id ASC"
         cursor.execute(query)
         states = cursor.fetchall()
