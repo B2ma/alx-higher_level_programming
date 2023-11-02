b4e1b918004f57031d2e34b81cefebe0ea2b88fb
B2ma <beatrice.terer@gmail.com>
2023-11-02 19:31:37 +0000

    Updated102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me, server to respond with You got me!
-curl -s -X POST -d "You got me!" -L http://0.0.0.0:5000/catch_me
+curl -s -o /dev/null -w "%{http_code}" -X POST -d "You got me!" http://0.0.0.0:5000/catch_me
