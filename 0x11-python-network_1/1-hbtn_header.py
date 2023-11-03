a4d381338f8efce8434d7d49136dd6a37315ae87
B2ma <beatrice.terer@gmail.com>
2023-11-03 16:11:48 +0000

    Created 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+Takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response
+"""
+import sys
+from urllib.request import urlopen
+
+url = sys.argv[1]
+with urlopen(url) as response:
+    x_request_id = response.getheader('X-Request-Id')
+    print(x_request_id)
+
+if __name__ == "__main__":
+    sys.exit(1)
