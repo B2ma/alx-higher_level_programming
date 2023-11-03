ec43294dd9f188344d6e5ee6e585001335e75d00
B2ma <beatrice.terer@gmail.com>
2023-11-03 19:26:43 +0000

    Updated 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,14 @@ the passed URL with the email as a parameter, and displays the
 body of the response (decoded in utf-8)
 """
 import sys
-from urllib import request,parse
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = parse.urlencode({'email': email}).encode('utf-8')
-    with request.urlopen(url, data) as response:
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
         print("Your email is: {}".format(body))
