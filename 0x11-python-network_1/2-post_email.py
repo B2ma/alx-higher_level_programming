8d96d4a2f0f6a25bff638892f7fbb2fce72b112a
B2ma <beatrice.terer@gmail.com>
2023-11-03 19:35:49 +0000

    Updated 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,4 +15,8 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
-        print("Your email is: {}".format(body))
+        extracted_email = None
+        parsed_response = urllib.parse.parse_qs(body)
+        if 'email' in parsed_response:
+            extracted_email = parsed_response['email'][0]
+        print("Your email is: {}".format(extracted_email))
