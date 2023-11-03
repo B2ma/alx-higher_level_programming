c8e4e568e915962697fc92f74c86079150de936b
B2ma <beatrice.terer@gmail.com>
2023-11-03 19:16:57 +0000

    Updated 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,9 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
-
 if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data)
