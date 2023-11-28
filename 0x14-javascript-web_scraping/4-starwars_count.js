0598d7906e2505db54002050e6835230774bdc58
B2ma <beatrice.terer@gmail.com>
2023-11-28 12:48:54 +0000

    Completes task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    // Parse the JSON response
+    const filmsData = JSON.parse(body);
+
+    // Filter films where "Wedge Antilles" is present
+    const filmsWithWedge = filmsData.results.filter(film =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
+
+    // Print the number of movies
+    console.log(filmsWithWedge.length);
+  }
+});
