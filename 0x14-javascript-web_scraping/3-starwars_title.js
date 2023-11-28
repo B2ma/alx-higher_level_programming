6890874e4b78ed31ac1ed8bbd2ea94ba02e407c7
B2ma <beatrice.terer@gmail.com>
2023-11-28 12:42:34 +0000

    removes semistandard error on task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, function(error, response, body) {
+request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
