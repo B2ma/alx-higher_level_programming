f13e525a796316b9722b3b0ae5e5aa0e18cbcae1
B2ma <beatrice.terer@gmail.com>
2023-12-06 09:53:56 +0000

    completes task 7 and 8

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+$(document).ready(function () {
+  // Make an AJAX request to fetch data from the SWAPI API
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    // Update the text content of the <div id="character"> with the character name
+    $('#character').text(data.name);
+  });
+});
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  // Make an AJAX request to fetch data from the SWAPI API
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    // Extract the array of movies from the fetched data
+    let movies = data.results;
+
+    // Iterate through the movies and append titles to the <ul id="list_movies">
+    movies.forEach(function (movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
+});
