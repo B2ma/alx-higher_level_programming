e3195061fe2aee0a3edde1c424f161499ef782c9
B2ma <beatrice.terer@gmail.com>
2023-12-06 10:03:01 +0000

    Completes task 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+$(document).ready(function () {
+  // Make an AJAX request to fetch data from the specified URL
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    // Update the text content of the <div id="hello"> with the fetched translation
+    $('#hello').text(data.hello);
+  });
+});
