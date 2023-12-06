e936924edc2a2aa1ea581c87bcedb5c47e4b7713
B2ma <beatrice.terer@gmail.com>
2023-12-06 09:14:13 +0000

    Completes task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  // Add a click event listener to the element with id "red_header"
+  $('#red_header').click(function () {
+    // Select the <header> element and add the class "red" to it
+    $('header').addClass('red');
+  });
+});
